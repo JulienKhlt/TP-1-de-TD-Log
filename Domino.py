2d815f3247bac842d2c7ae3e8365115e258bf6e6
Julien KHLAUT <julien.khlaut@eleves.enpc.fr>
2020-09-28 15:19:00 +0200

    Domino créé avec les dictionnaires

--- Domino.py
+++ Domino.py
@@ -5,29 +5,37 @@ class Domino :
 
     # TODO : remplacer par des dictionnaires
     def domino(self, a):
-        if (a == 1):
-            return [[" "]*5, [" ", " ", "*", " ", " "], [" "]*5]
+        if ( a==0 ):
+            return {}
+        elif (a == 1):
+            return {(2, 3): "*"}
         elif (a == 2):
-            return [["*", " ", " ", " ", " "], [" "]*5, [" ", " ", " ", " ", "*"]]
+            return {(1, 1 ): "*", (3, 5): "*"}
         elif (a == 3):
-            return [["*", " ", " ", " ", " "], [" ", " ", "*", " ", " "], [" ", " ", " ", " ", "*"]]
+            return {(1, 1): "*", (2, 3) : "*", (3, 5): "*"}
         elif (a == 4):
-            return [["*", " ", " ", " ", "*"], [" "]*5, ["*", " ", " ", " ", "*"]]
+            return {(1, 1): "*", (1, 5): "*", (3, 1): "*", (3, 5): "*"}
         elif (a == 5):
-            return [["*", " ", " ", " ", "*"], [" ", " ", "*", " ", " "], ["*", " ", " ", " ", "*"]]
+            return {(1, 1): "*", (1, 5): "*", (2, 3): "*", (3, 1): "*", (3, 5): "*"}
         elif (a == 6):
-            return [["*", " ", "*", " ", "*"], [" "]*5, ["*", " ", "*", " ", "*"]]
+            return {(1, 1): "*", (1, 3): "*", (1, 5): "*", (3, 1): "*", (3, 3): "*", (3, 5): "*"}
 
     def affichage(self):
         L1 = self.domino(self.n)
         L2 = self.domino(self.m)
         print("+-----|-----+")
-        for i in range(len(L1)):
+        for i in range(3):
             print("|", end="")
-            for j in range(len(L1[i])):
-                print(L1[i][j], end="")
+            for j in range(5):
+                if (i+1,j+1) in L1:
+                    print(L1[(i+1, j+1)], end="")
+                else:
+                    print(" ", end="")
             print("|", end="")
-            for j in range(len(L2[i])):
-                print(L2[i][j], end="")
+            for j in range(5):
+                if (i+1, j+1) in L2:
+                    print(L2[(i+1, j+1)], end="")
+                else:
+                    print(" ", end="")
             print("|")
         print("+-----|-----+")
